a998e8b1f78e015f25a0303afb3f1fc509980e87
keehan12 <keehanroberts@gmail.com>
2025-01-15 23:32:46 +1030

    Initial Release
    Unity project with example

--- Assets/scripts/source/flyCamera.cs
+++ Assets/scripts/source/flyCamera.cs
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:97138b3597503525b60afc407c553c0b345faa70d6521dbf6f0307820e61d7bd
-size 1014
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyCamera : MonoBehaviour
+{
+	public float climbSpeed = 4;
+	public float normalMoveSpeed = 10;
+	
+	void Start ()
+	{   
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}	
+	
+    void FixedUpdate ()
+	{
+		if (Input.GetMouseButton(1) || Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.Q)|| Input.GetKey (KeyCode.E))
+		{
+		transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+		transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+				
+			if (Input.GetKey (KeyCode.E))
+			{
+				transform.position += transform.up * (climbSpeed) * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.Q))
+			{
+				transform.position -= transform.up * (climbSpeed) * Time.deltaTime;
+			}
+		}
+	//end of update
+    }
+}
--- Assets/scripts/source/mouseLook.cs
+++ Assets/scripts/source/mouseLook.cs
@@ -1,3 +1,38 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8bdd476506f4059d51a3b71277d8c46eb44791fa9097200960d610368f9e0c77
-size 1028
+﻿using UnityEngine;
+ 
+ public class mouseLook : MonoBehaviour
+ {
+    private float X;
+    private float Y;
+    
+    public float SensitivityX = 64;
+	public float SensitivityY = 64;
+	public float MIN_X = 0;
+	public float MAX_X = 360;
+	public float MIN_Y = 90;
+	public float MAX_Y = -90;
+	
+    void Awake()
+    {
+        Vector3 euler = transform.rotation.eulerAngles;
+    }
+	
+	void Start()
+	{
+		X += Input.GetAxis("Mouse X") * (SensitivityX * Time.deltaTime);
+        Y -= Input.GetAxis("Mouse Y") * (SensitivityY * Time.deltaTime);
+	}
+     
+    void FixedUpdate()
+    {
+	
+        X += Input.GetAxis("Mouse X") * (SensitivityX * Time.deltaTime);
+		if (X < MIN_X) X += MAX_X;
+		else if (X > MAX_X) X -= MAX_X;
+		Y -= Input.GetAxis("Mouse Y") * (SensitivityY * Time.deltaTime);
+		if (Y < MIN_Y) Y = MIN_Y;
+		else if (Y > MAX_Y) Y = MAX_Y;
+		
+		transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(Y, X, 0.0f), Time.deltaTime * 32);
+	}
+    }
